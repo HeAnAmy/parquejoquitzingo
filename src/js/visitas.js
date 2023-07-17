9fddb94f0028f04e737daf4fdf3e5a1fe7e1fb32
Eduardo Ossio Reyes <eduardoossio99@gmail.com>
2023-07-17 17:55:26 -0600

    primer commit de la rama

---
+++ src/js/visitas.js
@@ -0,0 +1,14 @@
+const visitas = [
+    {
+        title:  
+        'EL PARQUEACTIVIDADESOTROS PARQUESSILOCENTRO DE ESTUDIOSCONTACTO10 AÑOS CELEBRACIÓN',
+        subtitulo: '¡Queridos amigos! ',
+        resto: '
+        Se llena mi corazón de alegría y agradecimiento al recordar el maravilloso día que compartimos, cuando la inauguración del Monolito en vuestro flamante Parque Joquicingo.
+        Todo confluyó para que sea un día extraordinario. Fue el encuentro, el abrazo y el intercambio con amigos entrañables. Fueron los cerros, los bosques eternos alrededor, los caminos convergiendo hacia el centro, hacia el nuevo Eje del Mundo que se alzaba. Fue el sol rodando desde los cerros por el valle, rebotando y repicando sobre los maizales... Oh, maizales! Sagrados y dorados, más valiosos aún que el oro mismo, para los ancestros sabios, para los abuelos que desde el fondo de los siglos se hicieron presentes a celebrar con nosotros... Allí estuvieron ellos con su espíritu, con su alma... y también encarnados en el cuerpo de niños, jóvenes y grandes, familias completas del municipio vecino que se trajeron el arco iris puesto en sus vestidos y adornos originarios; que con su música y su danza ancestrales inundaron el valle de ritmos, risas y canto.
+        Como dijimos en aquel momento, ese día fue enormemente significativo no sólo para los que estábamos allí. Sino también para los niños, para los jóvenes, para la cantidad de generaciones que desde ya y en los tiempos que vendrán, quien sabe cuántos siglos por delante, vayan encontrando en el Parque Joquicingo su lugar. Su lugar para reflexionar, para estudiar, para inspirarse, para conectarse con lo Profundo, para encontrarse entre sí en lo más hondo y sagrado de sus conciencias.
+        ¡Gracias mil, amigos queridos! Y los mejores deseos para que este nuevo faro, este nuevo sol del Mensaje de Silo, se continúe materializando en las mejores condiciones. Para beneficio de todos, en México y en NuestraAmérica, y para abrirle camino al ser humano de los nuevos tiempos.
+        ¡Paz, Fuerza y Alegría!
+        Hugo Novotny Parques de Estudio y Reflexión “Carcarañá”'
+    }
+]
